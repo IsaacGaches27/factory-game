6140f0d07c1268f8d777ba0c1cbdccd64144e4cc
IsaacGaches27 <163933350+IsaacGaches27@users.noreply.github.com>
2024-05-20 22:08:35 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,77 @@
-mod conveyor;
-mod item;
-mod container;
-
 use bevy::prelude::*;
-use crate::container::ItemContainer;
-use crate::conveyor::{ConveyorLogic, ConveyorPlugin, TailConveyor};
-use crate::item::Item;
+use bevy_ecs_tilemap::prelude::*;
+
+fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    let map_size = TilemapSize { x: 32, y: 32 };
+
+    // Layer 1
+    let mut tile_storage = TileStorage::empty(map_size);
+    let tilemap_entity = commands.spawn_empty().id();
+
+    fill_tilemap(
+        TileTextureIndex(3),
+        map_size,
+        TilemapId(tilemap_entity),
+        &mut commands,
+        &mut tile_storage,
+    );
+
+    let tile_size = TilemapTileSize { x: 15.0, y: 15.0 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::default();
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(asset_server.load("terrain.png")),
+        tile_size,
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 0.0),
+        ..Default::default()
+    });
+
+    // Layer 2
+    let mut tile_storage = TileStorage::empty(map_size);
+    let tilemap_entity = commands.spawn_empty().id();
+
+    fill_tilemap(
+        TileTextureIndex(2),
+        map_size,
+        TilemapId(tilemap_entity),
+        &mut commands,
+        &mut tile_storage,
+    );
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(asset_server.load("buildings.png")),
+        tile_size: TilemapTileSize { x: 15.0, y: 15.0 },
+        transform: get_tilemap_center_transform(&map_size, &grid_size, &map_type, 1.0),
+        ..Default::default()
+    });
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(ConveyorPlugin())
-        .add_systems(Startup, setup)
+        .add_plugins(DefaultPlugins.set(WindowPlugin{
+            primary_window: Some(Window {
+                title: String::from(
+                    "Basic Example",
+                ),
+                ..Default::default()
+            }),
+            ..default()
+        }).set(ImagePlugin::default_nearest()))
+        .add_plugins(TilemapPlugin)
+        .add_systems(Startup, startup)
         .run();
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
-
-    let item = commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("grass.png"),
-            transform: Transform::from_xyz(0., 0., 10.).with_scale(Vec3::splat(0.4)),
-            ..default()
-        }
-    )).insert(Item::default()).id();
-
-    let mut item_container = ItemContainer::default();
-    item_container.add_item(item);
-    item_container.set_block(true);
-
-    let conveyor = commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("conveyor.png"),
-            transform: Transform::from_xyz(0., 0., 0.),
-            ..default()
-        },
-        ConveyorLogic { incoming: None,timer: 0 },
-        item_container
-    )).id();
-
-    let item = commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("grass.png"),
-            transform: Transform::from_xyz(15., 0., 10.).with_scale(Vec3::splat(0.4)),
-            ..default()
-        }
-    )).insert(Item::default()).id();
-
-    let mut item_container = ItemContainer::default();
-    item_container.add_item(item);
-
-    let conveyor = commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("conveyor.png"),
-            transform: Transform::from_xyz(15., 0., 0.),
-            ..default()
-        },
-        ConveyorLogic { incoming: Some(conveyor),timer: 0 },
-        item_container,
-    )).id();
-
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("conveyor.png"),
-            transform: Transform::from_xyz(30., 0., 0.),
-            ..default()
-        },
-        ConveyorLogic { incoming: Some(conveyor),timer: 0 },
-        ItemContainer::default(),
-        TailConveyor()
-    ));
 }
